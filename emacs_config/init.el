02a11f3434f56f17ef025b8f0670757333538312
Ian Briggs <ianbriggsutah@gmail.com>
2023-12-20 18:58:21 -0700

    Update emacs config

--- emacs_config/init.el
+++ emacs_config/init.el
@@ -1,5 +1,5 @@
-;; Don't use this config if emacs version is below 27.2
-(if (version< emacs-version "27.2")
+;; Don't use this config if emacs version is below 29.1
+(if (version< emacs-version "29.1")
     (with-current-buffer " *load*"
       (goto-char (point-max))))
 
@@ -18,6 +18,7 @@
    afternoon-theme
    airline-themes
    markdown-mode
+   multiple-cursors
    powerline
    rust-mode
    scad-mode
@@ -53,49 +54,6 @@
 ;; Don't use stale files
 (setq load-prefer-newer t)
 
-;; Adds mouse support in terminal
-
-(require 'mwheel)
-(require 'mouse)
-(xterm-mouse-mode t)
-(mouse-wheel-mode t)
-
-;; (defun track-mode (e))
-;; (setq mouse-sel-mode t)
-
-;;(global-set-key (kbd "<mouse-4>") 'scroll-down-line)
-;;(global-set-key (kbd "<mouse-5>") 'scroll-up-line)
-
-;;(global-set-key [mouse-4] '(lambda () (interactive) (scroll-down 1)))
-;;(global-set-key [mouse-5] '(lambda () (interactive) (scroll-up 1)))
-
-;; (if (load "mwheel" t)
-;;     (mwheel-install))
-
-;; ;; turn on mouse wheel scrolling
-;; (defun sd-mousewheel-scroll-up (event)
-;;   "Scroll window under mouse up by five lines."
-;;   (interactive "e")
-;;   (let ((current-window (selected-window)))
-;;     (unwind-protect
-;;         (progn
-;;           (select-window (posn-window (event-start event)))
-;;           (scroll-up 2))
-;;       (select-window current-window))))
-
-;; (defun sd-mousewheel-scroll-down (event)
-;;   "Scroll window under mouse down by five lines."
-;;   (interactive "e")
-;;   (let ((current-window (selected-window)))
-;;     (unwind-protect
-;;         (progn
-;;           (select-window (posn-window (event-start event)))
-;;           (scroll-down 2))
-;;       (select-window current-window))))
-
-;; (global-set-key (kbd "<mouse-5>") 'sd-mousewheel-scroll-up)
-;; (global-set-key (kbd "<mouse-4>") 'sd-mousewheel-scroll-down)
-
 ;; Stop the backup files from being annoying
 (setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
       backup-by-copying t    ; Don't delink hardlinks
@@ -103,6 +61,43 @@
       delete-old-versions t  ; Automatically delete excess backups
       kept-new-versions 20   ; how many of the newest versions to keep
       kept-old-versions 5)   ; and how many of the old
+
+;; Based on https://blog.lambda.cx/posts/emacs-align-columns
+(defun align-non-space (BEG END)
+  "Aligns non-space columns seperated by 2 or more spaces"
+  (interactive "r")
+  (align-regexp BEG END "\\(\\s-\\s-\\s-*\\)\\S-+" 1 2 t))
+
+;; Programming defaults
+(add-hook 'prog-mode-hook 'display-fill-column-indicator-mode)
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+;; Things to include with markdown mode
+(add-hook 'markdown-mode-hook 'display-fill-column-indicator-mode)
+(add-hook 'markdown-mode-hook 'display-line-numbers-mode)
+(add-hook 'markdown-mode-hook 'flyspell-mode)
+
+;; Latex customizations
+(add-hook 'latex-mode-hook 'display-fill-column-indicator-mode)
+(add-hook 'latex-mode-hook 'display-line-numbers-mode)
+(add-hook 'latex-mode-hook 'flyspell-mode)
+(add-hook 'latex-mode-hook (lambda () (electric-indent-mode -1)))
+
+;; Things to include with markdown mode
+(add-hook 'rust-mode-hook (setq display-fill-column-indicator-column 100))
+
+;; Things to include with markdown mode
+(add-to-list 'auto-mode-alist '("\\.fpcore\\'" . scheme-mode))
+
+;; Use UTF-8 for latex symbols
+(setq org-pretty-entities t)
+
+;; Hide formatting markers, like _underline_
+(setf org-hide-emphasis-markers t)
+
+;; Things to include with org mode
+(add-hook 'org-mode-hook 'display-fill-column-indicator-mode)
+(add-hook 'org-mode-hook 'flyspell-mode)
 
 ;; Use a matching text theme
 (require 'afternoon-theme)
@@ -122,12 +117,6 @@
 
 ;; Silence bell
 (setq ring-bell-function 'ignore)
-
-;; Make vertical seperator pretty
-;; (set-display-table-slot standard-display-table 5 ?│)
-(set-display-table-slot standard-display-table 5 ? )
-;;(set-face-background 'vertical-border "gray")
-;;(set-face-foreground 'vertical-border (face-background 'vertical-border))
 
 ;; Set fill column to be 80
 (setq-default display-fill-column-indicator-column 80)
@@ -152,36 +141,37 @@
 ;; Always show trailing whitespace
 (setq-default show-trailing-whitespace t)
 
-;; Based on https://blog.lambda.cx/posts/emacs-align-columns
-(defun align-non-space (BEG END)
-  "Aligns non-space columns seperated by 2 or more spaces"
-  (interactive "r")
-  (align-regexp BEG END "\\(\\s-\\s-\\s-*\\)\\S-+" 1 2 t))
+;; multiple cursors!
+(use-package multiple-cursors
+  :bind ("C-c C-SPC" . mc/edit-lines))
 
-;; Programming defaults
-(add-hook 'prog-mode-hook 'display-fill-column-indicator-mode)
-(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+;; Configurations only for the terminal version
+(unless (display-graphic-p)
 
-;; Use UTF-8 for latex symbols
-(setq org-pretty-entities t)
+;; Adds mouse support in terminal
+(require 'mwheel)
+(require 'mouse)
+(xterm-mouse-mode t)
+(mouse-wheel-mode t)
 
-;; Hide formatting markers, like _underline_
-(setf org-hide-emphasis-markers t)
+;; Make vertical seperator pretty
+;; (set-display-table-slot standard-display-table 5 ?│)
+(set-display-table-slot standard-display-table 5 ? )
+;;(set-face-background 'vertical-border "gray")
+;;(set-face-foreground 'vertical-border (face-background 'vertical-border))
 
-;; Things to include with org mode
-(add-hook 'org-mode-hook 'display-fill-column-indicator-mode)
-(add-hook 'org-mode-hook 'flyspell-mode)
+)
 
-;; Things to include with markdown mode
-(add-hook 'markdown-mode-hook 'display-fill-column-indicator-mode)
-(add-hook 'markdown-mode-hook 'display-line-numbers-mode)
-(add-hook 'markdown-mode-hook 'flyspell-mode)
+;; Configurations only for the GUI version
+(when (display-graphic-p)
 
-;; Latex customizations
-(add-hook 'latex-mode-hook 'display-fill-column-indicator-mode)
-(add-hook 'latex-mode-hook 'display-line-numbers-mode)
-(add-hook 'latex-mode-hook 'flyspell-mode)
-(add-hook 'latex-mode-hook (lambda () (electric-indent-mode -1)))
+;; Remove GUI only top bar
+(tool-bar-mode -1)
 
-;; Things to include with markdown mode
-(add-hook 'rust-mode-hook (setq display-fill-column-indicator-column 100))
+;; Muscle memory from terminal, which can't differentiate C-- and C-_
+(bind-key "C--" 'undo)
+
+;; Smoother scrolling
+(pixel-scroll-precision-mode)
+
+)
